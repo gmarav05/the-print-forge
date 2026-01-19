24ae0be1a9609cae2c87fd913a5b937f29d4b9fb
Aravind <gmarav005@gmail.com>
2026-01-19 18:18:40 +0530

    Fix nostr web component typings and build errors

---
+++ app/types/nostr.d.ts
@@ -0,0 +1,36 @@
+import type * as React from 'react'
+
+declare global {
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        'nostr-zap-button': React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          npub: string
+          theme?: string
+          url?: string
+          text?: string
+          relays?: string | string[]
+        }
+
+        'nostr-profile-badge': React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          pubkey: string
+        }
+
+        'nostr-profile': React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          pubkey: string
+        }
+      }
+    }
+  }
+}
+
+export {}
